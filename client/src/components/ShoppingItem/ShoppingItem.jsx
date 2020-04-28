8a56137694c66e0b7901a1243064c158ca1727c3
mohammed mashrur rafique mia <mohammed.mia@ibm.com>
2020-04-28 16:14:54 +0100

    Check off of Shopping items

--- client/src/components/ShoppingItem/ShoppingItem.jsx
+++ client/src/components/ShoppingItem/ShoppingItem.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 
 class ShoppingItem extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {};
-    };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        let {name} = this.props;
+  render() {
+    let { name } = this.props;
+    let { check } = this.props;
 
-        return (
-          <div>{name}</div>
-        );
-    }
+    return (
+      <div>
+        <input type="checkbox" value={name} checked={check} />
+        <label>{name}</label>
+      </div>
+    );
+  }
 }
 
 export default ShoppingItem;
--- client/src/components/ShoppingItem/ShoppingItem.test.jsx
+++ client/src/components/ShoppingItem/ShoppingItem.test.jsx
@@ -8,17 +8,48 @@ configure({ adapter: new Adapter() });
 
 describe("ShoppingItem component", () => {
     let wrapper;
-    // const wrapper = mount(<ShoppingItem />);
 
     it("renders without crashing", () => {
         render(<ShoppingItem />);
     });
 
+    it('renders as an div', ()=> {
+        let wrapper = shallow(<ShoppingItem />);
+        expect(wrapper.type()).toBe('div');
+        expect(wrapper.childAt(0).type()).toBe('input');
+        expect(wrapper.childAt(1).type()).toBe('label');
+    });
+
+    it('renders as an input with type checkbox', ()=> {
+        let wrapper = shallow(<ShoppingItem />);
+        expect(wrapper.childAt(0).type()).toBe('input');
+        expect(wrapper.childAt(0).prop('type')).toBe('checkbox');
+    });
+
     it("should render the name of the item", () => {
         for (name of ["Milk", "Butter"]) {
-            wrapper = render(<ShoppingItem name={name} />);
-            expect(wrapper.text()).toEqual(name);
+            let wrapper = shallow(<ShoppingItem name={name} />);
+            expect(wrapper.childAt(0).prop('value')).toEqual(name);
+            expect(wrapper.childAt(1).text()).toEqual(name);
         }
+    });
+
+    it("should render the name of the item for checked item", () => {
+        for (name of ["Milk", "Butter"]) {
+            let wrapper = shallow(<ShoppingItem name={name} check="true"/>);
+            expect(wrapper.childAt(0).prop('value')).toEqual(name);
+            expect(wrapper.childAt(1).text()).toEqual(name);
+        }
+    });
+
+    it("should render a checkbox for an item", () => {
+        wrapper = shallow(<ShoppingItem name='Milk' check={true}/>);
+        expect(wrapper.childAt(0).prop('checked')).toBeTruthy();
+    });
+
+    it("should not render a checkbox for an item that is not checked", () => {
+        wrapper = shallow(<ShoppingItem name='Milk' check={false}/>);
+        expect(wrapper.childAt(0).prop('checked')).toBeFalsy();
     });
 
 });
