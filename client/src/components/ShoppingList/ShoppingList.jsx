a9d1dd9b7b9e2e5e658470095b4fc611769c0623
Sebastian Ribeiro <sebastianribeiro@Sebastians-MacBook-Pro.local>
2020-04-29 15:01:36 +0100

    add item

--- client/src/components/ShoppingList/ShoppingList.jsx
+++ client/src/components/ShoppingList/ShoppingList.jsx
@@ -4,20 +4,42 @@ import ShoppingItem from '../ShoppingItem/ShoppingItem'
 class ShoppingList extends Component {
     constructor(props) {
       super(props);
-      this.state = {};
-        
+      this.state = {
+        items: ['Milk', 'Banana', 'Apple'],
+        prov: "",
+      };
+
     };
 
-    render() {
-
-        return (
-          <div>
-            <ShoppingItem name={'Milk'}/>
-            <ShoppingItem name={'Banana'}/>
-            <ShoppingItem name={'Apple'}/>
-          </div>
-        );
+    handleChange = (e) => {
+      this.setState({
+        prov: e.target.value
+      })
     }
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+      this.setState({
+        items: [...this.state.items, this.state.prov]
+      })
+    }
+
+    render() {
+      const listOfItems = this.state.items.map((name, idx) => {
+        return <ShoppingItem key={idx} name={name} />
+      })
+      return (
+        <div>
+          {listOfItems}
+          <form onSubmit={this.handleSubmit}>
+            <label>Item:</label>
+            <input type="text" value={this.state.prov} onChange={this.handleChange} />
+            <button>Add Item</button>
+          </form>
+        </div>
+      );
+  }
+
 }
 
 export default ShoppingList;
